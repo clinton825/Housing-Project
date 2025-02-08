158e972623d4d4eef24d5b3a168ec8506eb821d4
clinton825 <20097793@mail.wit.ie>
2025-02-08 12:45:20 +0000

    Project Card Component Created

--- infrastructure/src/Pages/homePage.js
+++ infrastructure/src/Pages/homePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Grid from "@mui/material/Grid2";
+import ProjectCard from "../components/projectCard";
 
 const HomePage = (props) => {
   const projects = props.projects;
@@ -8,6 +9,9 @@ const HomePage = (props) => {
   <Grid container>
       <Grid size={12}>
           <h1> HomePage </h1>
+      </Grid>
+      <Grid size ={3}>
+        <ProjectCard project ={projects[0]} />
       </Grid>
     </Grid>
   );
---
+++ infrastructure/src/components/projectCard/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CardHeader from "@mui/material/CardHeader";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Grid from "@mui/material/Grid";
+import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import InfoIcon from "@mui/icons-material/Info";
+import img from "../../images/project-placeholder.png";
+
+export default function ProjectCard(props) {
+  const project = props.project;
+  return (
+    <Card>
+      <CardHeader title={project.planning_title} sx={{ textWrap: "nowrap" }} />
+      <CardMedia
+        sx={{ height: 250 }}
+        image={project.image ? project.image : img}
+      />
+      <CardContent>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <CalendarIcon fontSize="small" /> {project.planning_start_date}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <MonetizationOnIcon fontSize="small" /> {(project.planning_value / 1000000).toFixed(1)}M
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="body2" color="text.secondary">
+              {project.planning_description.substring(0, 100)}...
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+      <CardActions disableSpacing>
+        <Button variant="outlined" size="medium" color="primary" href={project.planning_url} target="_blank">
+          More Info
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
